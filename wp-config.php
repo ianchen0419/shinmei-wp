843f2c4b62ae7b2080a52de94cac5e7222a0e038
unknown <ianchen0419@gmail.com>
2019-06-30 14:17:26 +0800

    'multisite'

--- wp-config.php
+++ wp-config.php
@@ -83,6 +83,14 @@ $table_prefix = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/* Multisite */
+define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/shinmei/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 /* 編集が必要なのはここまでです ! WordPress でのパブリッシングをお楽しみください。 */
 
 /** Absolute path to the WordPress directory. */
