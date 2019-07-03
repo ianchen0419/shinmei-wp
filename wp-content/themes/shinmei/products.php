651ca6238d3d3c03cb1bbcf298a574393868b457
YianChen <ianchen0419@gmail.com>
2019-07-03 23:59:45 +0800

    multilang

--- wp-content/themes/shinmei/products.php
+++ wp-content/themes/shinmei/products.php
@@ -12,21 +12,21 @@
 	<div class="wrapper-size">
 		<ol class="contact-path">
 			<li>
-				<a href="<?php bloginfo('url') ?>">ホーム</a>
+				<a href="<?php bloginfo('url') ?>"><?php echo (get_current_blog_id()==1)?'ホーム':'Home' ?></a>
 			</li>
 			<li>
-				<a href="<?php bloginfo('url') ?>/products">製品一覧</a>
+				<a href="<?php bloginfo('url') ?>/products"><?php echo (get_current_blog_id()==1)?'製品一覧':'Products' ?></a>
 			</li>
 		</ol>
 		<a href="<?php bloginfo('url') ?>/products/thermal" class="products-item">
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_01.jpg" alt="包装機用サーマル プリンター" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>産業用包装機対応ダイレクトサーマル プリンター</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマル プリンター':'Industrial Direct Thermal Printer' ?></h2>
 				</div>
-				<p>産業用包装機対応ダイレクトサーマルプリンターは発売以来、産業用のプリンターとしてコンパクトなサイズとメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。</p>
+				<p><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマルプリンターは発売以来、産業用のプリンターとしてコンパクトなサイズとメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。':'Since its release, thermal printers for packaging machines have been used in many factories such as food, pharmaceuticals, chemicals, industrial, etc. due to their compact size and easy maintenance.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -34,11 +34,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_02.jpg" alt="卓上型サーマル プリンター" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>産業用卓上型ダイレクトサーマル プリンター</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'産業用卓上型ダイレクトサーマル プリンター':'Desktop Direct Thermal Printer' ?></h2>
 				</div>
-				<p>産業用卓上型ダイレクトサーマルプリンターは発売以来、空袋に直接印字する産業用ダイレクトサーマルプリンターとして設置とメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。</p>
+				<p><?php echo (get_current_blog_id()==1)?'産業用卓上型ダイレクトサーマルプリンターは発売以来、空袋に直接印字する産業用ダイレクトサーマルプリンターとして設置とメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。':'Since its release, desktop direct thermal printers have been used in many factories such as food, pharmaceutical, chemical, industrial and etc. due to easy installation and maintenance as an industrial direct thermal printer that prints directly on empty packaging.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -46,11 +46,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_03.jpg" alt="産業用オート ラベラー" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>産業用オート ラベラー</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'産業用オート ラベラー':'Industrial Auto Labeler' ?></h2>
 				</div>
-				<p>様々な対象に対し、自動でラベルを貼り付けます。簡易型、卓上型やライン設置型など、用途に応じたバリエーションがございます。</p>
+				<p><?php echo (get_current_blog_id()==1)?'様々な対象に対し、自動でラベルを貼り付けます。簡易型、卓上型やライン設置型など、用途に応じたバリエーションがございます。':'Automatically apply labels to various objects. We have various kinds of machines according to the application, such as simple type, desktop type and line installation type.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -58,11 +58,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_04.jpg" alt="チェックスタンパー" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>チェックスタンパー</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'チェックスタンパー':'Check Stamper' ?></h2>
 				</div>
-				<p>押印したい箇所に自動で押印を行います。卵パックに入っているサイズや日付が表記されている比較的小さなラベルからA3サイズのような大きなサイズの用紙まで幅広く押印業務を効率化します。<br>数量が増え手押しには限界を感じた方、常に安定したきれいな押印がしたい方におすすめです。</p>
+				<p><?php echo (get_current_blog_id()==1)?'押印したい箇所に自動で押印を行います。卵パックに入っているサイズや日付が表記されている比較的小さなラベルからA3サイズのような大きなサイズの用紙まで幅広く押印業務を効率化します。<br>数量が増え手押しには限界を感じた方、常に安定したきれいな押印がしたい方におすすめです。':'Stamp automatically on the targeted part. We improve the efficiency of stamping widely from relatively small labels that indicate the size and date in the egg pack to large-size paper such as A3 size.<br>It is recommended for those who exceed the capacity and want to have a stable and secure stamping.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -70,11 +70,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_05.jpg" alt="フィーダー/スタッカー" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>フィーダー/スタッカー</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'フィーダー/スタッカー':'Feeder / Stacker' ?></h2>
 				</div>
-				<p>ダイレクトサーマルプリンターやオートラベラーと連結し、軟包装の空袋の供給とストックを自動で行います。用途に合わせた様々なバリエーションをご用意しています。</p>
+				<p><?php echo (get_current_blog_id()==1)?'ダイレクトサーマルプリンターやオートラベラーと連結し、軟包装の空袋の供給とストックを自動で行います。用途に合わせた様々なバリエーションをご用意しています。':'Automatically feed empty soft packaging by connecting with direct thermal printer or auto labeler.<br>We offer various types suitable for your application.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -82,11 +82,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_06.jpg" alt="その他製品" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>その他製品</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'その他製品':'Other Products' ?></h2>
 				</div>
-				<p>その他シンメイが取り扱っている製品です。<br>鶏卵中身有無検査装置、ボトルラベラー、チケットプリンター</p>
+				<p><?php echo (get_current_blog_id()==1)?'その他シンメイが取り扱っている製品です。<br>鶏卵中身有無検査装置、ボトルラベラー、チケットプリンター':'Shinmei\'s other products: chicken egg content inspection device, bottle labeler, and ticket printer' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
@@ -94,11 +94,11 @@
 			<img src="<?php bloginfo('template_directory') ?>/inc/img/products/products_07.jpg" alt="消耗品" width="200" />
 			<div class="products-content">
 				<div class="deco-title">
-					<h2>消耗品</h2>
+					<h2><?php echo (get_current_blog_id()==1)?'消耗品':'Consumables' ?></h2>
 				</div>
-				<p>ラベル、インク、サーマルリボンなどの各種消耗品の取り扱いはこちらです。</p>
+				<p><?php echo (get_current_blog_id()==1)?'ラベル、インク、サーマルリボンなどの各種消耗品の取り扱いはこちらです。':'Various consumables such as labels, inks, and thermal ribbons.' ?></p>
 				<div class="products-button">
-					<button>詳細</button>
+					<button><?php echo (get_current_blog_id()==1)?'詳細':'Details' ?></button>
 				</div>
 			</div>
 		</a>
--- wp-content/themes/shinmei/thermal.php
+++ wp-content/themes/shinmei/thermal.php
@@ -2,70 +2,70 @@
 <?php get_header();?>
 
 <div id="visual">
-	<h1 class="visual-title">産業用包装機対応ダイレクトサーマル プリンター</h1>
+	<h1 class="visual-title"><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマル プリンター':'Direct Thermal Printer for Packaging Machine' ?></h1>
 </div>
 <main id="contact">
 	<div class="wrapper-size">
 		<ol class="contact-path">
 			<li>
-				<a href="<?php bloginfo('url') ?>">ホーム</a>
+				<a href="<?php bloginfo('url') ?>"><?php echo (get_current_blog_id()==1)?'ホーム':'Home' ?></a>
 			</li>
 			<li>
-				<a href="<?php bloginfo('url') ?>/products">製品一覧</a>
+				<a href="<?php bloginfo('url') ?>/products"><?php echo (get_current_blog_id()==1)?'製品一覧':'Products' ?></a>
 			</li>
 			<li>
-				<a href="<?php bloginfo('url') ?>/products/thermal">産業用包装機対応ダイレクトサーマル プリンター</a>
+				<a href="<?php bloginfo('url') ?>/products/thermal"><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマル プリンター':'Direct Thermal Printer for Packaging Machine' ?></a>
 			</li>
 		</ol>
 
 		<div class="section-title">
-			<h2>産業用包装機対応ダイレクトサーマル プリンター</h2>
+			<h2><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマル プリンター':'Direct Thermal Printer for Packaging Machine' ?></h2>
 		</div>
-		<p>産業用包装機対応ダイレクトサーマルプリンターは発売以来、産業用のプリンターとしてコンパクトなサイズとメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。</p>
+		<p><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマルプリンターは発売以来、産業用のプリンターとしてコンパクトなサイズとメンテナンスの手軽さから食品をはじめ医薬、化学、工業などの業界での多くの工場等でご愛用頂いております。':'Since its release, direct thermal printers for packaging machines have been used in many factories such as food, pharmaceuticals, chemicals, industrial, etc. due to their compact size and easy maintenance.' ?></p>
 		<div class="deco-title">
-			<h3>導入実績</h3>
+			<h3><?php echo (get_current_blog_id()==1)?'導入実績':'Installation' ?></h3>
 		</div>
 		<table class="clear-table">
 			<tbody>
 				<tr>
-					<th align="left" valign="top" width="15%">【食品業界】</th>
-					<td width="85%">パン、煮物、業務用食材、食品添加物、製茶、もやし、たけのこ、中華菓子、和菓子、冷凍食品、水産、燻製、レトルト、玄米、調理加工、米加工、コーヒー、ゼリー、養鶏、鶏卵、もずく、コロッケ、青果、洋生菓子、カステラ、食材、佃煮、フードチェーン、さつま揚げ、たれ、調味料、粉末食品、加工食品等</td>
+					<th align="left" valign="top" width="15%"><?php echo (get_current_blog_id()==1)?'【食品業界】':'[Food Industry]' ?></th>
+					<td width="85%"><?php echo (get_current_blog_id()==1)?'パン、煮物、業務用食材、食品添加物、製茶、もやし、たけのこ、中華菓子、和菓子、冷凍食品、水産、燻製、レトルト、玄米、調理加工、米加工、コーヒー、ゼリー、養鶏、鶏卵、もずく、コロッケ、青果、洋生菓子、カステラ、食材、佃煮、フードチェーン、さつま揚げ、たれ、調味料、粉末食品、加工食品等':'Bread, simmered food, food for business use, food additive, tea making, tea bean sprouts, bamboo shoots, Chinese confectionery, Japanese confectionery, frozen food, fishery, smoked, retort, brown rice, cooking processing, rice processing, coffee, jelly, chicken, chicken egg, mozuku, Croquettes, fruits and vegetables, confections, castellas, ingredients, boiled fish, food chain, fried sweets, pickles, seasonings, powdered foods, processed foods, etc.' ?></td>
 				</tr>
 				<tr>
-					<th align="left">【流通加工業界】</th>
-					<td>パッケージング等</td>
+					<th align="left"><?php echo (get_current_blog_id()==1)?'【流通加工業界】':'[Distribution Processing Industry]' ?></th>
+					<td><?php echo (get_current_blog_id()==1)?'パッケージング等':'Packaging etc.' ?></td>
 				</tr>
 				<tr>
-					<th align="left">【飲食店業界】</th>
-					<td>調味料、食材等</td>
+					<th align="left"><?php echo (get_current_blog_id()==1)?'【飲食店業界】':'[Restaurant Industry]' ?></th>
+					<td><?php echo (get_current_blog_id()==1)?'調味料、食材等':'Seasoning, ingredients etc' ?></td>
 				</tr>
 				<tr>
-					<th align="left">【医薬業界】</th>
-					<td>歯科材料等</td>
+					<th align="left"><?php echo (get_current_blog_id()==1)?'【医薬業界】':'[Medical Industry]' ?></th>
+					<td><?php echo (get_current_blog_id()==1)?'歯科材料等':'Dental materials' ?></td>
 				</tr>
 				<tr>
-					<th align="left">【化学業界】</th>
-					<td>化粧品等</td>
+					<th align="left"><?php echo (get_current_blog_id()==1)?'【化学業界】':'[Chemical Industry]' ?></th>
+					<td><?php echo (get_current_blog_id()==1)?'化粧品等':'Cosmetics etc.' ?></td>
 				</tr>
 				<tr>
-					<th align="left">【工業業界】</th>
-					<td>エレクトロニクス、プラスティック等</td>
+					<th align="left"><?php echo (get_current_blog_id()==1)?'【工業業界】':'[Manufacturing Industry]' ?></th>
+					<td><?php echo (get_current_blog_id()==1)?'エレクトロニクス、プラスティック等':'Electronics, plastics etc.' ?></td>
 				</tr>
 			</tbody>
 		</table>
 
 		<a href="<?php bloginfo('url') ?>/products/thermal/wrap" class="section-title detail">
-			<h2>産業用包装機対応ダイレクトサーマルプリンター SCS</h2>
+			<h2><?php echo (get_current_blog_id()==1)?'産業用包装機対応ダイレクトサーマルプリンター SCS':'Direct Thermal Printer for Packaging Machine SCS' ?></h2>
 		</a>
 		<div class="image-and-text reverse">
 			<div class="text-part">
-				<p>ラジアル制御機能付、連続、間欠式超小型プリンター。印字エリアにより1～3インチプリンターを選択。縦ピロー、横ピローなどに設置可能。コンパクトサイズで設置が容易、ランニングコストも低減。</p>
+				<p><?php echo (get_current_blog_id()==1)?'ラジアル制御機能付、連続、間欠式超小型プリンター。印字エリアにより1～3インチプリンターを選択。縦ピロー、横ピローなどに設置可能。コンパクトサイズで設置が容易、ランニングコストも低減。':'SCS is a continuous, intermittent microminiature printer with radial control function. Select a 1 to 3 inch printer according to the printing area. It can be installed on vertical pillows, horizontal pillows, etc. It is easy to install and reduce running cost due to its compact size.' ?></p>
 				<div class="button-area">
 					<a href="https://youtu.be/VoPl5sXoYU4" target="_blank" class="red-button">
 						<i class="fa fa-youtube-play"></i>
-						<span>動作状況</span>
+						<span><?php echo (get_current_blog_id()==1)?'動作状況':'Video' ?></span>
 					</a>
-					<a href="<?php bloginfo('url') ?>/material/inquiry-001" class="blue-button">お問い合わせ</a>
+					<a href="<?php bloginfo('url') ?>/material/inquiry-001" class="blue-button"><?php echo (get_current_blog_id()==1)?'お問い合わせ':'Inquiry' ?></a>
 				</div>
 			</div>
 			<div class="image-part">
