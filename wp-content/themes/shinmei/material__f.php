10dbba9b18cc789d4124c3c48f40e664c62c47fc
YianChen <ianchen0419@gmail.com>
2019-07-27 15:19:18 +0800

    folder split

--- wp-content/themes/shinmei/material__f.php
+++ wp-content/themes/shinmei/material__f.php
@@ -96,37 +96,37 @@
 			<h2>サーマルダイレクトプリンター</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data1'])?'':'disabled'; ?>" download="卓上型ダイレクトサーマルプリンター ＳＭＰ-３５０シリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data1'])?'':'disabled'; ?>" download="卓上型ダイレクトサーマルプリンター ＳＭＰ-３５０シリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>卓上型ダイレクトサーマルプリンター<br>ＳＭＰ-３５０シリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data2'])?'':'disabled'; ?>" download="卓上型一体ダイレクトサーマルプリンター ＡＦＢ-３５０シリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data2'])?'':'disabled'; ?>" download="卓上型一体ダイレクトサーマルプリンター ＡＦＢ-３５０シリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>卓上型一体ダイレクトサーマルプリンター<br>ＡＦＢ-３５０シリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data3'])?'':'disabled'; ?>" download="両面同時ダイレクトサーマルプリンター ＣＰＳ-ＵＢシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data3'])?'':'disabled'; ?>" download="両面同時ダイレクトサーマルプリンター ＣＰＳ-ＵＢシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>両面同時ダイレクトサーマルプリンター<br>ＣＰＳ-ＵＢシリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-4.pdf" class="blue-button download-button <?php echo isset($_POST['your_data4'])?'':'disabled'; ?>" download="小袋用ダイレクトサーマルプリンター ＣＰＳ-８５Ｅシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-4.pdf" class="blue-button download-button <?php echo isset($_POST['your_data4'])?'':'disabled'; ?>" download="小袋用ダイレクトサーマルプリンター ＣＰＳ-８５Ｅシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>小袋用ダイレクトサーマルプリンター<br>ＣＰＳ-８５Ｅシリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-5.pdf" class="blue-button download-button <?php echo isset($_POST['your_data5'])?'':'disabled'; ?>" download="ロールフィルムプリントシステム ＲＰＳシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-5.pdf" class="blue-button download-button <?php echo isset($_POST['your_data5'])?'':'disabled'; ?>" download="ロールフィルムプリントシステム ＲＰＳシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>ロールフィルムプリントシステム<br>ＲＰＳシリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-6.pdf" class="blue-button download-button <?php echo isset($_POST['your_data6'])?'':'disabled'; ?>" download="包装機対応ダイレクトサーマルプリンター ＳＣＳ-４１０シリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-6.pdf" class="blue-button download-button <?php echo isset($_POST['your_data6'])?'':'disabled'; ?>" download="包装機対応ダイレクトサーマルプリンター ＳＣＳ-４１０シリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>包装機対応ダイレクトサーマルプリンター<br>ＳＣＳ-４１０シリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/thermal-7.pdf" class="blue-button download-button <?php echo isset($_POST['your_data7'])?'':'disabled'; ?>" download="大袋用ダイレクトサーマルプリンター ＳＭＰ-６５０シリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/thermal-7.pdf" class="blue-button download-button <?php echo isset($_POST['your_data7'])?'':'disabled'; ?>" download="大袋用ダイレクトサーマルプリンター ＳＭＰ-６５０シリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>大袋用ダイレクトサーマルプリンター<br>ＳＭＰ-６５０シリーズ</span>
 			</a>
@@ -136,17 +136,17 @@
 			<h2>オートラベラー</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/labeler-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data8'])?'':'disabled'; ?>" download="卓上型空袋ラベラー ＡＬＳ-３５０Ｎシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/labeler-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data8'])?'':'disabled'; ?>" download="卓上型空袋ラベラー ＡＬＳ-３５０Ｎシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>卓上型空袋ラベラー<br>ＡＬＳ-３５０Ｎシリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/labeler-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data9'])?'':'disabled'; ?>" download="インラインオートラベリングシステム ＡＬＳ-１００Ⅱシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/labeler-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data9'])?'':'disabled'; ?>" download="インラインオートラベリングシステム ＡＬＳ-１００Ⅱシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>インラインオートラベリングシステム<br>ＡＬＳ-１００Ⅱシリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/labeler-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data10'])?'':'disabled'; ?>" download="ボトルラベラーK.I.2001">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/labeler-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data10'])?'':'disabled'; ?>" download="ボトルラベラーK.I.2001">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>ボトルラベラーK.I.2001</span>
 			</a>
@@ -156,21 +156,21 @@
 			<h2>鶏卵用</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/egg-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data11'])?'':'disabled'; ?>" download="鶏卵パック上貼り機 ＡＬＳ-１０００ＳＣＳシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/egg-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data11'])?'':'disabled'; ?>" download="鶏卵パック上貼り機 ＡＬＳ-１０００ＳＣＳシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>鶏卵パック上貼り機<br>ＡＬＳ-１０００ＳＣＳシリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/egg-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data12'])?'':'disabled'; ?>" download="インパックラベル日付印字プリンター ＣＰＳ-８５ＥⅢシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/egg-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data12'])?'':'disabled'; ?>" download="インパックラベル日付印字プリンター ＣＰＳ-８５ＥⅢシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>インパックラベル日付印字プリンター<br>ＣＰＳ-８５ＥⅢシリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/egg-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data13'])?'':'disabled'; ?>" download="鶏卵中身有無装置">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/egg-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data13'])?'':'disabled'; ?>" download="鶏卵中身有無装置">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>鶏卵中身有無装置</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/egg-4.pdf" class="blue-button download-button <?php echo isset($_POST['your_data14'])?'':'disabled'; ?>" download="鶏卵ライン用日付検査カメラ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/egg-4.pdf" class="blue-button download-button <?php echo isset($_POST['your_data14'])?'':'disabled'; ?>" download="鶏卵ライン用日付検査カメラ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>鶏卵ライン用日付検査カメラ</span>
 			</a>
@@ -180,7 +180,7 @@
 			<h2>フィーダー・スタッカー</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/feeder-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data15'])?'':'disabled'; ?>" download="フィーダー・スタッカー">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/feeder-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data15'])?'':'disabled'; ?>" download="フィーダー・スタッカー">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>フィーダー・スタッカー</span>
 			</a>
@@ -190,17 +190,17 @@
 			<h2>チェックスタンパー</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/stamper-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data16'])?'':'disabled'; ?>" download="自動汎用計数押印機 ＣＳシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/stamper-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data16'])?'':'disabled'; ?>" download="自動汎用計数押印機 ＣＳシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>自動汎用計数押印機<br>ＣＳシリーズ</span>
 			</a>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/stamper-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data17'])?'':'disabled'; ?>" download="手形振出・裏書用押印機 ＣＳ-ＢⅡシリーズ">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/stamper-2.pdf" class="blue-button download-button <?php echo isset($_POST['your_data17'])?'':'disabled'; ?>" download="手形振出・裏書用押印機 ＣＳ-ＢⅡシリーズ">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>手形振出・裏書用押印機<br>ＣＳ-ＢⅡシリーズ</span>
 			</a>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/stamper-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data18'])?'':'disabled'; ?>" download="パンフレットスタンパー">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/stamper-3.pdf" class="blue-button download-button <?php echo isset($_POST['your_data18'])?'':'disabled'; ?>" download="パンフレットスタンパー">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>パンフレットスタンパー</span>
 			</a>
@@ -210,7 +210,7 @@
 			<h2>その他</h2>
 		</div>
 		<div>
-			<a href="<?php bloginfo('template_directory') ?>/inc/download/others-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data19'])?'':'disabled'; ?>" download="ラベル / シール">
+			<a href="<?php bloginfo('template_directory') ?>/inc/download/jp/others-1.pdf" class="blue-button download-button <?php echo isset($_POST['your_data19'])?'':'disabled'; ?>" download="ラベル / シール">
 				<i class="fa fa-file-text-o fa-fw"></i>
 				<span>ラベル / シール</span>
 			</a>
