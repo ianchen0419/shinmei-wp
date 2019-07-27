7fdf658732fd2d8a2a60530420b2b50694c941cd
unknown <ianchen0419@gmail.com>
2019-07-27 14:51:34 +0800

    'fix'

--- wp-content/themes/shinmei/functions.php
+++ wp-content/themes/shinmei/functions.php
@@ -1,19 +1,19 @@
 <?php 
 
-function twentyfourteen_wp_title( $title) {
-	if (is_home() || is_front_page()) {
+function twentyfourteen_wp_title( $title, $sep ) {
+	global $paged, $page;
+	if (is_home() || is_front_page() || is_404()) {
 		if(get_current_blog_id()==1){
 			$title='産業用印字表示ソリューションのシンメイ';
 		}else{
 			$title='Shinmei Co.Ltd.: Marketing & Innovating for Industrial Printing Solution';
 		}
 	}else{
-		$title = get_bloginfo( 'name', 'display' );
+		$title=$title.get_bloginfo( 'name', 'display' );
 	}
 
 	return $title;
 }
-
 add_filter( 'wp_title', 'twentyfourteen_wp_title', 10, 2 );
 
 ?>
